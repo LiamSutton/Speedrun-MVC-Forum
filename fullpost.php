83f2a6e1b2c81e43824d5775936c45e1b750f8f6
Liam Sutton <liamsutton1997@outlook.com>
2019-11-28 09:26:24 +0000

    Buttons / Modals now only shown to logged in users

--- fullpost.php
+++ fullpost.php
@@ -1,10 +1,12 @@
 <?php
+session_start();
 require_once ("Models/PostDataset.php");
 $dataset = new PostDataset();
 
 $view = new stdClass();
 $id = $_GET['id'];
 $view->pageTitle = "Post "  .$id;
+// TODO: Why is the main post treated differently than replies? they are the same thing.
 $view->mainPost = $dataset->getPost($id);
 $view->replies = $dataset->getReplies($id);
 require_once ("Views/fullpost.phtml");
