e224a3a90307cfc8801d3cdd3c33eb3626c47351
Liam Sutton <liamsutton1997@outlook.com>
2019-12-03 14:05:50 +0000

    refactored ReCaptcha into a helper class, create user functioning using it

---
+++ Models/ReCaptcha.php
@@ -0,0 +1,22 @@
+<?php
+
+class ReCaptcha
+{
+    public function __construct()
+    {
+
+    }
+
+    public static function getReCaptchaResult($response)
+    {
+        if (strlen($response) == 0)
+        {
+            return false;
+        }
+    
+        $result = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6Lc0psUUAAAAAOuaEeY6Q4mEEzPwOaZnPKz2FUbG&response=".$response);
+        $resultData = json_decode($result, true);
+
+        return $resultData['success'];
+    }
+}
--- createuser.php
+++ createuser.php
@@ -1,26 +1,37 @@
 <?php
+
+require_once("Models/ReCaptcha.php");
 require_once("Models/UserDataset.php");
 session_start();
 if (isset($_POST['submit'])) {
     if (isset($_POST['g-recaptcha-response'])) {
-        $resp = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6Lc0psUUAAAAAOuaEeY6Q4mEEzPwOaZnPKz2FUbG&response=" . $_POST['g-recaptcha-response']);
-        $respData = json_decode($resp, true);
-        // Getting response from google, continue if successful // end if false
-        if ($respData['success']) {
+
+        $reCaptchaResult = ReCaptcha::getReCaptchaResult($_POST['g-recaptcha-response']);
+
+        // User validated from reCaptcha
+        if ($reCaptchaResult)
+        {
+            // Setup user details
             $username = $_POST['username'];
             $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
             $firstname = $_POST['firstname'];
             $lastname = $_POST['lastname'];
 
+            // Instanciate data class
             $userDataset = new UserDataset();
-            $userDataset->createUser($username, $password, $firstname, $lastname, 'https://robohash.org/test');
 
-            // Log User In
+            // Insert new user into the db
+            $userDataset->createUser($username, $password, $firstname, $lastname, 'https://robohash.org/test');
+            
+            // Log the new user in
             $_SESSION['loggedIn'] = true;
             $_SESSION['username'] = $username;
             $_SESSION['id'] = $userDataset->getUser($username)->getId();
+
+            // redirect to index page
             header("Location: index.php");
         }
+        // User failed ReCaptcha
         else
         {
             die("Captcha failed");
