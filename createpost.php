d5e53b0fbfae250968b47179d1ed89dd22fed51e
Liam Sutton <liamsutton1997@outlook.com>
2019-12-03 15:01:16 +0000

    ReCaptcha added to create post

--- createpost.php
+++ createpost.php
@@ -2,19 +2,43 @@
 session_start();
 require_once ("Models/UserDataset.php");
 require_once ("Models/PostDataset.php");
+require_once("Models/ReCaptcha.php");
 
 if (isset($_POST['submit']))
 {
-    $userDataset = new UserDataset();
-    $postsDataset = new PostDataset();
-    // Get User
-    $user = $userDataset->getUser($_SESSION['username']);
+    // User completed the reCaptcha
+    if (strlen($_POST['g-recaptcha-response']) != 0)
+    {
+        $reCaptchaResult = ReCaptcha::getReCaptchaResult($_POST['g-recaptcha-response']);
 
-    $posterID = $user->getId();
-    $title = $_POST['title'];
-    $content = $_POST['content'];
-    // Commit it to DB
-    $postsDataset->createPost($posterID, $title, $content);
+        //User successfully completed ReCaptcha
+        if ($reCaptchaResult)
+        {
+            // Instanciate user and post data objects
+            $userDataset = new UserDataset();
+            $postsDataset = new PostDataset();
+            
+            // Get User
+            $user = $userDataset->getUser($_SESSION['username']);
+            
+            // get data required to create a Post
+            $posterID = $user->getId();
+            $title = $_POST['title'];
+            $content = $_POST['content'];
+
+            // Commit it to DB
+            $postsDataset->createPost($posterID, $title, $content);
+        }
+        else
+        {
+            die("ReCaptcha Failed");
+        }
+    }
+    else
+    {
+        die("ReCaptcha must be completed");
+    }
+
     // Redirect
     header("Location: posts.php");
 }
