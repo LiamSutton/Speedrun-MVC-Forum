70db5e3cdfb9efbc63fffb4c3e532f0294259702
Liam Sutton <liamsutton1997@outlook.com>
2019-12-04 17:46:36 +0000

    Users can now upload an avatar when creating their account

--- createuser.php
+++ createuser.php
@@ -6,13 +6,34 @@ session_start();
 if (isset($_POST['submit'])) {
     
     // User completed ReCaptcha
-    if (strlen($_POST['g-recaptcha-result']) != 0) {
+    if (strlen($_POST['g-recaptcha-response']) != 0) {
 
         $reCaptchaResult = ReCaptcha::getReCaptchaResult($_POST['g-recaptcha-response']);
 
         // User validated from reCaptcha
         if ($reCaptchaResult)
         {
+            $avatar = "";
+            
+            if ($_FILES['user_avatar']['name'] != 'none')
+            {
+                $avatar = $_FILES['user_avatar']['name'];
+                $dir = 'images/';
+                $uploadfile = $dir . basename($_FILES['user_avatar']['name']);
+                // TODO: Add Validation, Error Handling
+                if (move_uploaded_file($_FILES['user_avatar']['tmp_name'], $uploadfile))
+                {
+                    echo "<h1>File uploaded and moved</h1>";
+                }
+                else
+                {
+                    echo $_FILES['user_avatar']['error'];
+                }
+            }
+            else
+            {
+                $avatar = 'default_avatar.jpg';
+            }
             // Setup user details
             $username = $_POST['username'];
             $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
@@ -23,7 +44,7 @@ if (isset($_POST['submit'])) {
             $userDataset = new UserDataset();
 
             // Insert new user into the db
-            $userDataset->createUser($username, $password, $firstname, $lastname, 'https://robohash.org/test');
+            $userDataset->createUser($username, $password, $firstname, $lastname, $avatar);
             
             // Log the new user in
             $_SESSION['loggedIn'] = true;
