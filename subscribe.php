97224fbd93a4eabe802dcf67661764bd853bcfeb
Liam Sutton <liamsutton1997@outlook.com>
2019-12-05 17:20:19 +0000

    Subscribing / unsubscribing from either post or watchlist redirects to correct location

--- subscribe.php
+++ subscribe.php
@@ -6,7 +6,15 @@ $postData = new PostDataset();
 
 $userID = $_SESSION['id'];
 $postID = $_GET['id'];
+$from = $_GET['from'];
 
 $postData->addToWatchlist($userID, $postID);
 
-header("Location: fullpost.php?id=$postID");
+if ($from == "Watchlist")
+{
+    header("Location: watchlist.php?id=$userID");
+}
+else
+{
+    header("Location: fullpost.php?id=$postID");
+}
--- unsubscribe.php
+++ unsubscribe.php
@@ -5,8 +5,15 @@ require_once ("Models/PostDataset.php");
 $postData = new PostDataset();
 $userID = $_SESSION['id'];
 $postID = $_GET['id'];
-
+$from = $_GET['from'];
 $postData->removeFromWatchlist($userID, $postID);
 
 // TODO: should be able to unsubscribe from post AND Watchlist page, need to sort out redirect
-header("Location: fullpost.php?id=$postID");
+if ($from == "Watchlist")
+{
+    header("Location: watchlist.php?id=$userID");
+}
+else
+{
+    header("Location: fullpost.php?id=$postID");
+}
