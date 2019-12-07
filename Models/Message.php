1c18f9cd70fb7619cc00a2a83ac7321d417217dc
Liam Sutton <liamsutton1997@outlook.com>
2019-12-07 12:03:12 +0000

    began to implement MessageData model

--- Models/Message.php
+++ Models/Message.php
@@ -9,4 +9,29 @@ class Message
     {
 
     }
+
+    public static function Message($dbRow)
+    {
+        $instance = new self();
+
+        $instance->_messageID = $dbRow['m_id'];
+        $instance->_messageSenderID = $dbRow['m_senderID'];
+        $instance->_messageRecipientID = $dbRow['m_recipientID'];
+        $instance->_messageContent = $dbRow['m_content'];
+    }
+
+    public function getMessageID()
+    {
+        return $this->_messageID;
+    }
+
+    public function getMessageSenderID()
+    {
+        return $this->_messageSenderID;
+    }
+
+    public function getMessageRecipientID()
+    {
+        return $this->_messageRecipientID;
+    }
 }
--- Models/MessageData.php
+++ Models/MessageData.php
@@ -1,7 +1,16 @@
 <?php
 
+require_once("Models/Database.php");
 
 class MessageData
 {
+    protected $_dbHandle, $_dbInstance;
+
+    public function __construct()
+    {
+        $this->_dbInstance = Database::getInstance();
+        $this->_dbHandle = $this->_dbInstance->getConnection();
+    }
+
 
 }
