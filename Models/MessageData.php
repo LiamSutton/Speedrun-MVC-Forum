947c0aed39ccdbc99e41d961afb087559dd503de
Liam Sutton <liamsutton1997@outlook.com>
2019-12-12 14:51:59 +0000

    documented MessageData Class

--- Models/MessageData.php
+++ Models/MessageData.php
@@ -3,16 +3,34 @@
 require_once("Models/Database.php");
 require_once ("Models/Message.php");
 
+/**
+ * Class MessageData
+ */
 class MessageData
 {
+    /**
+     * @var PDO
+     */
+    /**
+     * @var Database|PDO|null
+     */
     protected $_dbHandle, $_dbInstance;
 
+    /**
+     * MessageData constructor.
+     */
     public function __construct()
     {
         $this->_dbInstance = Database::getInstance();
         $this->_dbHandle = $this->_dbInstance->getConnection();
     }
 
+    // i know this isnt worth any marks but i built it before i got your email saying you just need to hide contact detils
+    // feels-bad-man.jpg
+    /**
+     * @param $userID - the id of the logged in user to retrieve messages for
+     * @return array - an array of Messages sent to the user
+     */
     public function getRecievedMessages($userID)
     {
         $sqlQuery = "SELECT m.m_id, m.m_senderID, m.m_recipientID, m.m_content, s.u_username as 'sender', r.u_username as 'recipient'
@@ -38,6 +56,10 @@ class MessageData
         return $data;
     }
 
+    /**
+     * @param $userID - the id of the logged in user who sent the messages
+     * @return array - an array of messages the user sent
+     */
     public function getSentMessages($userID)
     {
         $sqlQuery = "SELECT m.m_id, m.m_senderID, m.m_recipientID, m.m_content, s.u_username as 'sender', r.u_username as 'recipient'
