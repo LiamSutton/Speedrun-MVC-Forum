a40c59c89904c0e061efb192cc047ed75634f0c9
Liam Sutton <liamsutton1997@outlook.com>
2019-12-12 14:41:57 +0000

    documented categoryData

--- Models/CategoryData.php
+++ Models/CategoryData.php
@@ -3,16 +3,31 @@
 require_once("Models/Database.php");
 require_once("Models/Category.php");
 
+/**
+ * Class CategoryData
+ */
 class CategoryData
 {
+    /**
+     * @var PDO
+     */
+    /**
+     * @var Database|PDO|null
+     */
     protected $_dbHandle, $_dbInstance;
 
+    /**
+     * CategoryData constructor.
+     */
     public function __construct()
     {
         $this->_dbInstance = Database::getInstance();
         $this->_dbHandle = $this->_dbInstance->getConnection();
     }
 
+    /**
+     * @return array - an array containing all the current categories for the forum
+     */
     public function getAllCategories()
     {
         $sqlQuery = "SELECT c_id, c_name, COUNT(p_id) AS c_postCount
@@ -33,6 +48,10 @@ class CategoryData
         return $data;
     }
 
+    /**
+     * @param $id - the id of the category we want
+     * @return mixed - the name of the category of ID = $id
+     */
     public function getCategoryName($id)
     {
         $sqlQuery = "SELECT c_name FROM Categories WHERE c_id = ?";
