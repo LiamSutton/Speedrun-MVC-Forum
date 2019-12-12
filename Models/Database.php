bd4c8fcecdc752ab3c8056f561d6a64261d6d9d5
Liam Sutton <liamsutton1997@outlook.com>
2019-12-12 14:44:34 +0000

    commented Database

--- Models/Database.php
+++ Models/Database.php
@@ -1,12 +1,29 @@
 <?php
 
 
+/**
+ * Class Database
+ */
 class Database
 {
+    /**
+     * @var null
+     */
     protected static $_dbInstance = null;
+    /**
+     * @var PDO
+     */
     protected $_dbHandle;
 
     // Creates a new PDO Object relating to the database
+
+    /**
+     * Database constructor.
+     * @param $username - username of the DB
+     * @param $password - Password to access the DB
+     * @param $host - the hostname for the DB
+     * @param $dbname - the Name of the DB
+     */
     public function __construct($username, $password, $host, $dbname)
     {
         try
@@ -20,9 +37,14 @@ class Database
     }
 
     // If there is an active instance of the database retrieve it, else create a new instance and retrieve it
+
+    /**
+     * @return Database|null - uses the singleton pattern to either create an instance of a DB or return a current one
+     */
     public static function getInstance()
     {
         $username = "sgb959";
+        // not my paypal password
         $password = "MaxLex2018!";
         $host = "poseidon.salford.ac.uk";
         $dbname = "sgb959_forum";
@@ -34,11 +56,17 @@ class Database
         return self::$_dbInstance;
     }
 
+    /**
+     * @return PDO - a connection to the current instance
+     */
     public function getConnection()
     {
         return $this->_dbHandle;
     }
 
+    /**
+     * RELEASE THE Kracke... DB
+     */
     public function destruct()
     {
         $this->_dbHandle = null;
