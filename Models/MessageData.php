c1b1cf94ab417ee8aa48d56627c3dd3ae9208e21
Liam Sutton <liamsutton1997@outlook.com>
2020-04-18 16:11:12 +0100

    Can get a list of conversations where a user has been sent a message by another user odered by unopened and time last message sent JANKY KILL IT WITH FIRE

--- Models/MessageData.php
+++ Models/MessageData.php
@@ -82,20 +82,20 @@ class MessageData
         $this->_dbInstance->destruct();
     }
 
-    /** This function gets a list of all users that the currently logged in user has had conversations with
+    /** This function gets a list of all users that the currently logged in user has recieved messages from and orders them by the number of unopened messages and the
+     * time the last message was sent
      * @param $id: the id of the logged in user
      * @return array: an associative array of all the messages
      */
     public function getConversationList($id) {
-        $sqlQuery = "SELECT DISTINCT u_id as id, u_username as username, CONCAT(u_firstname, ' ', u_lastname) as fullname
-                     FROM Users
-                     JOIN Messages M on Users.u_id = M.m_recipientID
-                     WHERE m_senderID = :id
-                     UNION
-                     SELECT DISTINCT u_id as id, u_username as username, CONCAT(u_firstname, ' ', u_lastname) as fullname
-                     FROM Users
-                     JOIN Messages M on Users.u_id = M.m_senderID
-                     WHERE m_recipientID = :id";
+        $sqlQuery = "SELECT DISTINCT u_id as id,
+                u_username as username,
+                concat (u_firstname, ' ', u_lastname) as fullname,
+                (SELECT count(m_id) FROM Messages where m_senderID = u_id AND m_recipientID = :id and m_opened = false) as unopened,
+                (SELECT MAX(m_datecreated) from Messages where m_senderID = u_id and m_recipientID = :id) as lastmsg
+                FROM Users
+                WHERE (SELECT count(m_id) FROM Messages where m_senderID = u_id and m_recipientID = :id) > 0
+                order by unopened desc , lastmsg desc";
         
         $statement = $this->_dbHandle->prepare($sqlQuery);
         $statement->bindValue(":id", $id, PDO::PARAM_INT);
